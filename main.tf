0a1f7419b59c5e222e19463c67d91e053c50c34d
ruthvikgNEU <garlapati.r@northeastern.edu>
2023-02-16 10:47:34 -0500

    Profile Should be given dynamically Code added

--- main.tf
+++ main.tf
@@ -1,29 +1,31 @@
 resource "aws_vpc" "vpc" {
   cidr_block = var.cidr_block
-
   tags = {
-    Name = "VPC"
+    Name = "VPC ${var.vpc_id}"
   }
 }
 
+data "aws_availability_zones" "all" {
+  state = "available"
+}
+
 resource "aws_subnet" "public_subnet" {
-  count                   = length(var.public_subnet)
+  count                   = var.public_subnet
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public_subnet[count.index]
-  availability_zone       = var.public_availability_zones[count.index]
-  map_public_ip_on_launch = false
+  cidr_block              = cidrsubnet(var.cidr_block, 8, count.index)
+  availability_zone       = element(data.aws_availability_zones.all.names, count.index % length(data.aws_availability_zones.all.names))
+  map_public_ip_on_launch = "true"
 
   tags = {
-    Name = "Public subnet ${count.index + 1}"
+    Name = "Public subnet ${count.index + 1} - VPC ${var.vpc_id}"
   }
 }
 
 resource "aws_subnet" "private_subnet" {
-  count                   = length(var.private_subnet)
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private_subnet[count.index]
-  availability_zone       = var.private_availability_zones[count.index]
-  map_public_ip_on_launch = false
+  count             = var.private_subnet
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 4, count.index + 1)
+  availability_zone = element(data.aws_availability_zones.all.names, count.index % length(data.aws_availability_zones.all.names))
 
   tags = {
     Name = "Private subnet ${count.index + 1}"
@@ -60,13 +62,13 @@ resource "aws_route_table" "private_route_table" {
 }
 
 resource "aws_route_table_association" "aws_public_route_table_association" {
-  count          = length(var.public_subnet)
+  count          = var.public_subnet
   subnet_id      = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_route_table_association" "aws_private_route_table_association" {
-  count          = length(var.private_subnet)
+  count          = var.private_subnet
   subnet_id      = aws_subnet.private_subnet[count.index].id
   route_table_id = aws_route_table.private_route_table.id
 }
