e458d58327de9e92b7736ba110d73e685712b349
ruthvikgNEU <garlapati.r@northeastern.edu>
2023-04-13 16:25:21 -0400

    Final Code Added

--- main.tf
+++ main.tf
@@ -80,14 +80,6 @@ resource "aws_security_group" "application" {
   description = "Allow TLS inbound/outbound traffic"
   vpc_id      = aws_vpc.vpc.id
 
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
 
   ingress {
     description     = "TLS from VPC"
